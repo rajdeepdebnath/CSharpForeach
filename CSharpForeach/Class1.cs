c58d26016cec1b5b96589e572aed4aa6845f7a73
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2020-02-15 01:57:38 +0530

    initial commit

---
+++ CSharpForeach/Class1.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//namespace CSharpForeach
+//{
+//    class MyList
+//    {
+//        private List<int> list = new List<int>();
+//        public object Current { get { return list[index]; } }
+//        private int index;
+
+//        public MyList()
+//        {
+//            index = -1;
+//        }
+
+//        public void Add(int item)
+//        {
+//            list.Add(item);
+//        }
+//        public int Count()
+//        {
+//            return list.Count;
+//        }
+
+//        public MyList GetEnumerator()
+//        {
+//            return this;
+//        }
+
+//        public bool MoveNext()
+//        {
+//            index++;
+//            var check_index = index < this.list.Count;
+//            return check_index;
+//        }
+
+//        public void Reset()
+//        {
+//            this.list = new List<int>();
+//        }
+//    }
+//    class Program1
+//    {
+//        static void Main1(string[] args)
+//        {
+//            var mylist = new MyList();
+//            mylist.Add(1);
+//            mylist.Add(2);
+
+//            foreach (var item in mylist)
+//            {
+//                Console.WriteLine(item);
+//            }
+
+//            Console.WriteLine("Done...");
+//        }
+//    }
+//}
---
+++ CSharpForeach/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpForeach
+{
+    class MyType
+    {
+        private List<int> list = new List<int>();
+        public object Current { get { return list[current_index]; } }
+        private int current_index;
+
+        public MyType()
+        {
+            current_index = -1;
+        }
+        public void Add(int item)
+        {
+            list.Add(item);
+        }
+
+        public MyType GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            current_index++;
+            return current_index < list.Count;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mytype = new MyType();
+            mytype.Add(1);
+            mytype.Add(2);
+
+            foreach (var item in mytype)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Done...");
+        }
+    }
+}
